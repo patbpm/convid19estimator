cedd5f4c10ed91a5c0d55f12187453a514bfc5a6
Patrick Bope <patbpm1@gmail.com>
2020-04-13 21:33:28 +0200

    fixing of static files

--- convid19estimator/settings.py
+++ convid19estimator/settings.py
@@ -127,13 +127,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Extra lookup directories for collectstatic to find static files
-# STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'static'),
-# )
+#  Extra lookup directories for collectstatic to find static files
+#STATICFILES_DIRS = (
+   # os.path.join(BASE_DIR, 'static'),
+#)
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
